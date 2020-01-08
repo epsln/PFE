d791bca284a61f4d157f0b7a16ff56589a5583d1
BaptisteHudyma <bapt.hudyma95650@gmail.com>
2020-01-08 13:22:57 +0100

    Good arretes detection, locations cleaned

--- codes/utils/find.py
+++ codes/utils/find.py
@@ -41,9 +41,15 @@ def clean_title (_list) :
 def find_arretes (txt) :
 #TODO : complete arrete names search
 	ref_arretes = re.findall (r"\d{2,3}-\d{4}-\d{2}-\d{2}-\d{1,4}", txt)
+
+	#find all arrete refs with format "arrete [...] AB/123.12-ABC/...
+	arretes = re.findall(r"(?i)(?<=\barrete)(((?:[. *](?:prefectoral)?(?:ministeriel)?|n.)*?)(\w{2,8}(?:[\/_.-]\w{1,5}){1,5})\b){1,2}", txt)
+	#only take third capturing group
+	arretes = [ref [2] for ref in arretes]
 	
+
 	return remove_same (
-				ref_arretes
+				ref_arretes + arretes
 			)
 
 def find_date_publi (dates) :
@@ -74,7 +80,6 @@ def find_dates (txt) :
 
 def clean_dates_string (date_list) :
 	#return dates "DD month YYYY" as format "YYYY-MM-DD"
-	#return [dateparser.parse(date_string).date().strftime ('%Y-%m-%d') for date_string in date_list]
 	return_list = []
 	for date_string in date_list :
 		try :
@@ -89,7 +94,6 @@ def clean_dates_string (date_list) :
 
 def clean_dates_slash (date_list) :
 	#return dates "DD/MM/YYYY" as format "YYYY-MM-DD"
-	#return [datetime.strptime (date_string, "%d/%m/%Y").strftime ('%Y-%m-%d') for date_string in date_list]
 	return_list = []
 	for date_string in date_list :
 		try :
@@ -97,19 +101,20 @@ def clean_dates_slash (date_list) :
 			return_list.append (val.strftime ('%Y-%m-%d'))
 
 		except :
-			#wrong date orthographe
+			#wrong date orthographe : maybe year is 97 instead of 1997
 			try :
 				val = datetime.strptime (date_string, "%d/%m/%y")
 				return_list.append (val.strftime ('%Y-%m-%d'))
 			except :
+				#distance pour trouver orthographe correct ?
 				print ("Wrong date writing : ", date_string)
 	return return_list
 
 def find_titles (txt) :
-	#find "Arreté" and the all characters (limit is 600 char) until "préfet", "arrêté", "page" or "vu"
+	#find "arrete" and the all characters (limit is 600 char) until "prefet", "arrete", "page" or "vu"
 	titles = list ()
-	titles = re.findall (r"(?i)((?<=(?<!présent )arr[eê]t[eé]).{1,300}?(?=(pr[ée]fet\b|article\b|(?<!présent )arr[eê]t[ée]|page|vu\b)))", txt)
-	spaces = re.compile (r" {1,}")	#detect multiple following spaces
+	titles = re.findall (r"(?i)((?<=(?<!present )arrete).{1,300}?(?=(prefet\b|article\b|(?<!present )arrete|page|vu\b)))", txt)
+	spaces = re.compile (r" {1,}")					#detect multiple following spaces
 	titles = [spaces.sub(' ', title[0]) for title in titles]	#remove all double spaces
 	return remove_same (titles)
 
@@ -120,21 +125,21 @@ def find_raa (txt) :
 
 def find_articles (txt) :
 	#find "R. 3-827", "L16-98-893-1"
-	articles = re.findall (r"[RADL](?:[. *])*\d{1,4}(?:[.-]\d{1,4}(?:[-.]\d{1,4})*)", txt)
-	pattern = re.compile (r"[^0-9-RADL]") #all element that are not numbers or - or R A D and L
+	articles = re.findall (r"(?i)\b[radl](?:[. *])*\d{1,4}(?:[.-]\d{1,4}(?:[-.]\d{1,4})*)", txt)
+	pattern = re.compile (r"(?i)[^0-9-radl]") 			#all element that are not numbers or - or R A D and L
 	return remove_same ([pattern.sub ('', art) for art in articles])
 
 def find_lois (txt) :
-	#find "Loi(s)" and the following numbers 
+	#find "loi(s)" and the following numbers 
 	return remove_same (remove_keep_nb (
-				re.findall (r"[lL]ois?(?:.){0,4}\d{1,4}-\d{1,4}", txt)
+				re.findall (r"(?i)lois?(?:.){0,4}\d{1,4}-\d{1,4}", txt)
 				)
 			)
 
 def find_decrets (txt) :
-	#find "Décret(s) and the following numbers
+	#find "decret(s) and the following numbers
 	return remove_same (remove_keep_nb (
-				re.findall (r"[dD][ée]crets?(?:.){0,5}\d{1,4}-\d{1,4}", txt)
+				re.findall (r"(?i)decrets?(?:.){0,5}\d{1,4}-\d{1,4}", txt)
 					)
 			)
 
@@ -148,7 +153,7 @@ def find_names (txt) :
 	#remove monsieur/madame/m/mme/m./mme./
 	remove = re.compile (r"(?i)(?<=\b)(monsieur |madame |m[ \.]+|mme[ \.]+|DDT\b ?)")
 	
-	#remove words shorter than 2 caracs and words longer than 20 char
+	#remove words shorter than 2 characters and words longer than 20 char
 	short = re.compile (r"\W*\b\w{1,2}\b|\W*\b\w{20,}\b")
 
 	#remove multiple spaces
@@ -190,14 +195,24 @@ def find_locs (txt) :
 	#remove isolated numbers (keep only number with letters ahead)
 	remove = r"(?i)\b[a-z]+\d+|\b[a-z -]+\b"
 
+	#remove words shorter than 2 caracs and words longer than 20 char
+	short = re.compile (r"\W*\b\w\b|\W*\b\w{20,}\b")
+
+	#remove arrete, monsieur/madame, cedex, 
+	remove_specials = re.compile (r"(?i)(?<=\b)(monsieur|madame|arrete|cedex|)")
+	
+	
 	#remove multiple spaces
 	spaces = re.compile (r" {1,}")
 
 	for ent in nlp (txt).ents: 
 		if "LOC" in ent.label_ :	#Personne
 			loc = "".join (re.findall (remove, ent.text.replace ("_", " ")))
-			loc = spaces.sub (' ', clean.sub ('', loc))
-			if len (loc) > 2 :
+			#insert space before every majuscules
+			loc = re.sub(r"(?<=\w)([A-Z])", r" \1", clean.sub ('', loc))
+			loc = spaces.sub (' ', short.sub("", remove_specials.sub ("", loc)))
+
+			if len (loc) > 3 :
 				locs.append (loc)
 
 	return remove_same (locs)
