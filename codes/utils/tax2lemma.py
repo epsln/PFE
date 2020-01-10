522f50531ca6b0687c71757a6201c37aef0fdc22
smallito <martin.olivier1997@gmail.com>
2020-01-10 17:34:35 +0100

    fixed taxonomy

--- codes/utils/tax2lemma.py
+++ codes/utils/tax2lemma.py
@@ -1,18 +1,23 @@
 import spacy
 import re
 
-outFile = open("taxonomieLemma.txt", "w")
+outFile = open("test.txt", "w")
 nlp = spacy.load('fr_core_news_md')
+i = 0
 with open("taxonomieUTF.txt") as taxo:
     for line in taxo:
+        i+=1
+        #print(round(float(i)/len(taxo.read())*100, 3),"%")
+        print(i)
         lineTreated = re.findall(r'".+?"', line)[0].replace('"',"")
-        lineTreated = line.strip('"').strip(',')
+        lineTreated = line.strip('"').strip(',').lower()
         tokens = nlp(lineTreated)
-        outLine = line
+        outLine = line.lower()
         for word in tokens:
             if word.is_stop:
-                outLine = outLine.strip(str(word))
+                outLine = outLine.replace(str(word), "")
             if not word.is_stop:
                 outLine = outLine.replace(str(word), str(word.lemma_))
+        outLine = outLine.replace("  ", " ")
         outFile.write(outLine)
                 
