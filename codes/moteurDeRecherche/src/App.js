46f41aafa4c6a69f9ceefbbced10cb6107e38a97
Staberlin <valentain@et.esiea.fr>
2020-01-13 17:04:41 +0100

    ajout des images et reduction des sizes de donnees dans les filtres

--- codes/moteurDeRecherche/src/App.js
+++ codes/moteurDeRecherche/src/App.js
@@ -7,29 +7,44 @@ import {
   MultiList,
   SelectedFilters,
   ReactiveList,
-  RangeSlider
+  RangeSlider,
 } from "@appbaseio/reactivesearch";
-import { Row, Button, Col, Card} from "antd";
+import { Row, Button, Col, Card, Switch, Tree, Popover, Affix } from "antd";
 import "antd/dist/antd.css";
+
+function getNestedValue(obj, path) {
+  const keys = path.split(".");
+  const currentObject = obj;
+  const nestedValue = keys.reduce((value, key) => {
+    if (value) {
+      return value[key];
+    }
+    return "";
+  }, currentObject);
+  if (typeof nestedValue === "object") {
+    return JSON.stringify(nestedValue);
+  }
+  return nestedValue;
+}
 
 
 function renderItem(res, triggerClickAnalytics) {
   var title = ""
   var url = ""
   if (res.raa.length <= 0)
-	  title = "Pas un RAA";
+    title = "Pas un RAA";
   else
-	  title = "RAA n° " + res.raa[0];
+    title = "RAA n° " + res.raa[0];
   
   if (res.taxo.length > 0)
-	  var taxo = "Classement taxonomique : ";
-	  for (var i=0; i < res.taxo.length && i < 3; i++)
-		  taxo += res.taxo[i] + ", ";
+    var taxo = "Classement taxonomique : ";
+    for (var i=0; i < res.taxo.length && i < 3; i++)
+      taxo += res.taxo[i] + ", ";
 
   var publi = res.publi.split ("-");
   var date = ""
   if (publi.length === 3)
-  	date = "Date de publication: " + publi[2] + "/" + publi[1] + "/" + publi[0];
+    date = "Date de publication: " + publi[2] + "/" + publi[1] + "/" + publi[0];
 
   return (
     <Row
@@ -39,13 +54,13 @@ function renderItem(res, triggerClickAnalytics) {
       key={res._id}
       style={{ margin: "20px auto", borderBottom: "1px solid #ededed" }}
     >
-      <Col span={24}>
-        <h3> <font size="+2">
-		{title}
-	</font> </h3>
-	<div><span className="publi-date">{date}</span> </div>
-	<div> {taxo} </div>
-      </Col>
+    <Col span={24}>
+      <h3> <font size="+2">
+      {title}
+      </font> </h3>
+      <div><span className="publi-date">{date}</span> </div>
+      <div> {taxo} </div>
+    </Col>
       <div style={{ padding: "20px" }}>
         {url ? (
           <Button
@@ -60,6 +75,7 @@ function renderItem(res, triggerClickAnalytics) {
   );
 }
 
+
 const App = () => (
   <ReactiveBase
     app="prefetapp"
@@ -68,107 +84,115 @@ const App = () => (
     analytics={true}
     searchStateHeader
   >
+
     <Row gutter={16} style={{ padding: 20 }}>
       <Col span={5}>
         <Card>
           <MultiList
-            componentId="raaList"
-	    showCount={false}
+            componentId="raa"
             dataField="raa.keyword"
             queryFormat="or"
             sortBy="desc"
-            size={10000}
+            size={100}
             style={{
               marginBottom: 20
             }}
             title="Références RAA"
           />
-	
           <RangeSlider
-            componentId="datesPubliSlider"
+            componentId="dates_publi"
             dataField="publi.keyword"
             queryFormat="and"
+            size={100}
             style={{
               marginBottom: 40
             }}
-	    range={{
-              start: 1950,
+            range={{
+              start: 1900,
               end: 2050
-	    }}
-	    rangeLabels={{
-              start: "1950",
+            }}
+            rangeLabels={{
+              start: "1900",
               end: "2050"
-	    }}
+            }}
+            stepValue={1}
             showHistogram={true}
             showFilter={true}
-            title="Date de publication"
+            interval={2}
+            title="Dates de publications"
           />
-
           <MultiList
-            componentId="taxonomieList"
+            componentId="taxonomie"
             dataField="taxo.keyword"
             queryFormat="and"
             size={100}
             style={{
               marginBottom: 40
             }}
-	    react={{and: [
-	      	"raaList",
-		"arretesList",
-		"datesPubliSlider",
-		"searchBar"
-	    	]
-	    }}
             title="Taxonomie"
           />
           <MultiList
-            componentId="arretesList"
+            componentId="arretes"
             dataField="arretes.keyword"
             queryFormat="and"
             size={100}
             style={{
               marginBottom: 20
             }}
-	    react={{and: [
-		"taxonomieList", 
-	      	"raaList",
-		"datesPubliSlider",
-		"searchBar"
-	    	]
-	    }}
             title="Arretes"
           />
         </Card>
       </Col>
       <Col span={18}>
+        <div className="navbar">
+          <div className="logo-container">
+            <img
+                className="app-logo"
+                src="https://www.origne53.fr/images/PREFECTURE.jpg"
+                alt="LogoPrefeture"
+                width="150" 
+                height="100"
+            />
+            <img
+                className="app-logo"
+                src="https://www.esiea.fr/wp-content/uploads/2016/04/Logo-ESIEA.jpg"
+                alt="LogoEsiea"
+                width="200" 
+                height="100"
+                align="right"
+            />
+          </div>
+        </div>
         <DataSearch
           autosuggest={true}
-          componentId="searchBar"
-	  react={{
-	    and: [
-	     "taxonomieList", 
-	     "raaList",
-	     "arretesList",
-	     "datesPubliSlider",
-	     ]
-            }}
+          componentId="search"
           dataField={[
-            "raa",
-            "publi",
             "arretes",
+            "articles",
             "dates",
-            "titres",
             "decrets",
+            "lieux",
             "lois",
             "noms",
-            "articles",
+            "orgs",
+            "publi",
+            "raa",
             "taxo",
-            "lieux",
-            "orgs"
+            "titres"
           ]}
           fieldWeights={[
-            10,
-            5,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
             1,
             1,
             1,
@@ -180,45 +204,39 @@ const App = () => (
             1,
             1,
             1
-	  ]}
-	  debounce={100}
-          fuzziness="AUTO"
+          ]}
+          fuzziness={1}
+          highlight={false}
+          highlightField={[
+            "arretes",
+            "articles",
+            "dates",
+            "decrets",
+            "lieux",
+            "lois",
+            "noms",
+            "orgs",
+            "publi",
+            "raa",
+            "taxo",
+            "titres"
+          ]}
           queryFormat="and"
           size={10}
           style={{
             marginBottom: 20
           }}
-          title="Recueil Search Engine"
-	  renderError={error => (
-	      <div>
-	      Something went wrong with DataSearch
-		<br />
-	        Error details
-		<br />
-	        {error}
-		</div>
-	   )}
-	   sortOptions={["Test", "publi", "asc"]}
+          title="Receuil Search Engine"
         />
 
-        <SelectedFilters
-		showClearAll={true}
-		clearAllLabel="Clear filters"
-	/>
+        <SelectedFilters />
         <div id="result">
           <ReactiveList
             componentId="result"
             dataField="_score"
             pagination={true}
-	    loader="Loading Results.."
             react={{
-              and: [
-		"taxonomieList", 
-	      	"raaList",
-		"arretesList",
-		"datesPubliSlider",
-		"searchBar"
-		]
+              and: ["taxonomie", "raa", "arretes", "dates_publi", "search"]
             }}
             renderItem={renderItem}
             size={10}
